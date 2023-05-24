5fe2131758731a84f3e253eb8a940b26b20c9d89
Lam Dinh <lamson.dev@gmail.com>
2023-05-24 10:50:13 +0700

    first commit

---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#update maximum map count check for Elasticsearch
+#https://www.elastic.co/guide/en/elasticsearch/reference/current/_maximum_map_count_check.html
+
+
+sysctl -w vm.max_map_count=262144
+
+docker compose up -d \
+	elasticsearch \
+	kibana \
+	mysql \
+	mongo \
+	redis \
+	memcached \
+	rabbitmq \
+
